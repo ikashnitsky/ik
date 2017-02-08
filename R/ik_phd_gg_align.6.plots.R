05439b2f316c2f89c0ffa69d0df5cf46c20bc879
Ilya Kashnitsky <ilya.kashnitsky@gmail.com>
2017-02-08 13:45:39 +0100

    six plots

--- R/ik_phd_gg_align.6.plots.R
+++ R/ik_phd_gg_align.6.plots.R
@@ -16,19 +16,19 @@ ik_phd_gg_align.6.plots <- function(list.plots, labels=LETTERS[1:6],labels.size=
                 coord_equal(xlim = c(0, 21), ylim = c(0, 30), expand = c(0,0))+
 
                 annotation_custom(ggplotGrob(list.plots[[1]]),
-                                  xmin = 0, xmax = 8, ymin = 21, ymax = 29)+
+                                  xmin = 0.5, xmax = 8.5, ymin = 21, ymax = 29)+
 
                 annotation_custom(ggplotGrob(list.plots[[2]]),
-                                  xmin = 13, xmax = 21, ymin = 19.5, ymax = 27.5)+
+                                  xmin = 12.5, xmax = 20.5, ymin = 19.5, ymax = 27.5)+
                 annotation_custom(ggplotGrob(list.plots[[3]]),
-                                  xmin = 13,xmax = 21,ymin = 10.5,ymax = 18.5)+
+                                  xmin = 12.5,xmax = 20.5,ymin = 10.5,ymax = 18.5)+
 
                 annotation_custom(ggplotGrob(list.plots[[4]]),
-                                  xmin = 0, xmax = 8, ymin = 9,ymax = 17)+
+                                  xmin = 0.5, xmax = 8.5, ymin = 9,ymax = 17)+
                 annotation_custom(ggplotGrob(list.plots[[5]]),
-                                  xmin = 0, xmax = 8, ymin = 0, ymax = 8)+
+                                  xmin = 0.5, xmax = 8.5, ymin = 0, ymax = 8)+
                 annotation_custom(ggplotGrob(list.plots[[6]]),
-                                  xmin = 13,xmax = 21, ymin = 0, ymax = 8)+
+                                  xmin = 12.5,xmax = 20.5, ymin = 0, ymax = 8)+
 
                 labs(x = NULL, y = NULL)+
                 theme_void()
@@ -36,9 +36,9 @@ ik_phd_gg_align.6.plots <- function(list.plots, labels=LETTERS[1:6],labels.size=
 
         # DF with the coordinates of the 5 arrows
         df.arrows <- data.frame(id=1:5,
-                                x=c(8,8,13,13,13),
+                                x=c(8.5,8.5,12.5,12.5,12.5),
                                 y=c(21,21,10.5,10.5,10.5),
-                                xend=c(13,13,8,8,13),
+                                xend=c(12.5,12.5,8.5,8.5,12.5),
                                 yend=c(20.5,17.5,10,7,7))
 
         # add arrows
@@ -66,7 +66,7 @@ ik_phd_gg_align.6.plots <- function(list.plots, labels=LETTERS[1:6],labels.size=
 
         # add labes
         gg <- gg + annotate('text',label = labels,
-                            x=c(0,13,13,0,0,13)+.5,
+                            x=c(.5,12.5,12.5,.5,.5,12.5)+.5,
                             y=c(29,27.5,18.5,17,8,8)+.1,
                             size=labels.size,hjust=0, vjust=0)
 
